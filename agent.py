2b84cf354cb66d4ec6dd862cd4fe8053998b04f4
ShamserulAwsGeek <shamserul786@gmail.com>
2025-06-08 21:04:18 +0530

    added file

--- agent.py
+++ agent.py
@@ -1,5 +1,6 @@
 from strands import Agent
 from strands.tools.mcp import MCPClient
 from strands_tools import http_request
+from mcp import stdio_client, StdioServerParametrs
 
-
+#Define naming focused system prompt:
