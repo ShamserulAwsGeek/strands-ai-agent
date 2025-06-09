c9bee7f3cf4d09707e4272c5ce84bb4a135185de
ShamserulAwsGeek <shamserul786@gmail.com>
2025-06-09 22:42:39 +0530

    prompt added

--- agent.py
+++ agent.py
@@ -4,3 +4,6 @@ from strands_tools import http_request
 from mcp import stdio_client, StdioServerParametrs
 
 #Define naming focused system prompt:
+NAMING_SYSTEM_PROMPT = """
+You are an assitant that helps to name open source projects.
+
